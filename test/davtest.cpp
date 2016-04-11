2e8eaca255a931704d8e11e673c2781197199394
David Tenty <daltenty@gmail.com>
2016-04-11 05:54:54 -0400

    More unit test

--- test/davtest.cpp
+++ test/davtest.cpp
@@ -6,9 +6,11 @@
 #include "../globals.h"
 #include "../fsoperations.h"
 #include "../helper.h"
+#include "../diskstructures.h"
 
 class FSTest : public ::testing::Test {
 protected:
+    char inbuffer[BLOCKSIZE]="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc laoreet lobortis pharetra. Nam convallis feugiat tellus, vitae aliquet est placerat ac. Nam nec interdum risus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aenean in mi tristique, pharetra lectus quis, pharetra mi. Duis ultrices, lacus ac mattis posuere, velit leo vulputate ante, sit amet egestas enim orci sodales risus. Fusce aliquam pharetra viverra. Duis gravida ex in metus suscipit, eu mattis lectus vulputate. Etiam eu amet";
     int disk;
     char *tempfile;
     const int blockcount=800;
@@ -42,6 +44,19 @@ protected:
         close(disk);
     }
 
+    blockptr *dupfat() {
+        blockptr *clone=(blockptr*)malloc(fatsize*BLOCKSIZE);
+        memcpy(clone,fat,fatsize*BLOCKSIZE);
+        return clone;
+    }
+
+    void validatefat(blockptr *expectedfat) {
+        for (int i=0; i < super.numblocks; i++) {
+            SCOPED_TRACE(i);
+            EXPECT_EQ(expectedfat[i],fat[i]);
+        }
+    }
+
     ~FSTest() {
         free(tempfile);
     }
@@ -56,7 +71,7 @@ TEST_F(FSTest,DirectoryCreation) {
 
 TEST_F(FSTest,ReadWriteFile) {
     fuse_file_info file;
-    char inbuffer[BLOCKSIZE]="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc laoreet lobortis pharetra. Nam convallis feugiat tellus, vitae aliquet est placerat ac. Nam nec interdum risus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aenean in mi tristique, pharetra lectus quis, pharetra mi. Duis ultrices, lacus ac mattis posuere, velit leo vulputate ante, sit amet egestas enim orci sodales risus. Fusce aliquam pharetra viverra. Duis gravida ex in metus suscipit, eu mattis lectus vulputate. Etiam eu amet";
+
     char outbuffer[BLOCKSIZE];
 
     ASSERT_NO_FATAL_FAILURE(initdisk());
@@ -66,5 +81,44 @@ TEST_F(FSTest,ReadWriteFile) {
     ASSERT_EQ(davfs_read("/foo.txt",outbuffer,BLOCKSIZE,0,&file),BLOCKSIZE);
     ASSERT_EQ(davfs_release("/foo.txt",&file),0);
     ASSERT_STREQ(inbuffer,outbuffer);
+    unmountdisk();
+}
+
+TEST_F(FSTest,AddRemoveDirectory) {
+    ASSERT_NO_FATAL_FAILURE(initdisk());
+    ASSERT_NO_FATAL_FAILURE(mountdisk());
+    blockptr *beforefat=dupfat();
+    ASSERT_EQ(davfs_mkdir("/foo",777),0) << strerror(errno);
+    ASSERT_EQ(davfs_rmdir("/foo"),0) << strerror(errno);
+    validatefat(beforefat);
+    free(beforefat);
+    unmountdisk();
+}
+
+TEST_F(FSTest,AddRemoveFile) {
+    fuse_file_info file;
+    ASSERT_NO_FATAL_FAILURE(initdisk());
+    ASSERT_NO_FATAL_FAILURE(mountdisk());
+    blockptr *beforefat=dupfat();
+    ASSERT_EQ(davfs_create("/foo.txt",777,&file),0) << strerror(errno);
+    ASSERT_EQ(davfs_release("/foo.txt",&file),0) << strerror(errno);
+    ASSERT_EQ(davfs_unlink("/foo.txt"),0) << strerror(errno);
+    validatefat(beforefat);
+    free(beforefat);
+    unmountdisk();
+}
+
+TEST_F(FSTest,AddRemoveFileWithData) {
+    fuse_file_info file;
+    ASSERT_NO_FATAL_FAILURE(initdisk());
+    ASSERT_NO_FATAL_FAILURE(mountdisk());
+    blockptr *beforefat=dupfat();
+    ASSERT_EQ(davfs_create("/foo.txt",777,&file),0) << strerror(errno);
+    ASSERT_EQ(davfs_write("/foo.txt",inbuffer,BLOCKSIZE,0,&file),BLOCKSIZE);
+    ASSERT_EQ(davfs_write("/foo.txt",inbuffer,BLOCKSIZE,512,&file),BLOCKSIZE);
+    ASSERT_EQ(davfs_release("/foo.txt",&file),0) << strerror(errno);
+    ASSERT_EQ(davfs_unlink("/foo.txt"),0) << strerror(errno);
+    validatefat(beforefat);
+    free(beforefat);
     unmountdisk();
 }
